ce509e64280f24010c53c500ac5b2d79880dd9b1
tiago-goncalves-a22201250 <148215009+tiago-goncalves-a22201250@users.noreply.github.com>
2024-10-31 16:19:48 +0000

    Feito ate a alteração do fundo

---
+++ lab7/main.js
@@ -0,0 +1,50 @@
+
+//1º exe
+const textoPassa = document.getElementById("passa");
+
+textoPassa.onmouseover = function () {
+    textoPassa.textContent = "1. Obrigado por passares!";
+}
+textoPassa.onmouseout = function () {
+    textoPassa.textContent = "1. Passa por aqui!";
+}
+
+//2º exe
+const paragrafo = document.getElementById("cor");
+const botaoRed = document.getElementById("red");
+const botaoGreen = document.getElementById("green");
+const botaoBlue = document.getElementById("blue");
+
+botaoRed.onclick = function() {
+    paragrafo.style.color = "red";
+}
+
+botaoGreen.onclick = function() {
+    paragrafo.style.color = "green";
+}
+
+botaoBlue.onclick = function() {
+    paragrafo.style.color = "blue";
+}
+
+//3º exe
+const texto = document.getElementById("txt");
+
+texto.addEventListener("input",function() {
+
+    const textLength = txt.value.length;
+
+    if (textLength === 0) {
+        txt.style.backgroundColor = "white";
+    } else if (textLength % 3 === 1) {
+        txt.style.backgroundColor = "cyan";
+    } else if (textLength % 3 === 2) {
+        txt.style.backgroundColor = "red";
+    } else {
+        txt.style.backgroundColor = "yellow";
+    }
+});
+
+//4º exe
+const fundo = document.getElementById("txtFundo");
+
