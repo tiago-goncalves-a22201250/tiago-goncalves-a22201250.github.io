d0ea4546d37a0b5f039caf75908c73ecc81bf8b4
tiago-goncalves-a22201250 <148215009+tiago-goncalves-a22201250@users.noreply.github.com>
2024-11-15 15:28:44 +0000

    Produtos feito

--- lab9/main.js
+++ lab9/main.js
@@ -1,16 +1,44 @@
-const produtosCon = document.getElementById("produtosContainer");
+document.addEventListener('DOMContentLoaded', function() {
+    carregarProdutos(produtos);
+  });
+  
+  function carregarProdutos(produtos) {
+    const productsContainer = document.getElementById('produtosContainer');
+    productsContainer.innerHTML = '';
+  
+    produtos.forEach(produto => {
+      console.log(produto);           
+      console.log(produto.id);    
+      console.log(produto.title); 
+  
+      const article = criarProduto(produto);
+      productsContainer.appendChild(article);
+    });
+  }
+  
+  function criarProduto(produto) {
+    const article = document.createElement('article');
+    article.classList.add('produto');
+  
+    const titleElement = document.createElement('h3');
+    titleElement.textContent = produto.title;
+  
+    const imageElement = document.createElement('img');
+    imageElement.src = produto.image;
+    imageElement.alt = produto.title;
+  
+    const priceElement = document.createElement('p');
+    priceElement.textContent = `Custo total: ${produto.price.toFixed(2)} €`;
+    priceElement.classList.add('preco');
 
-produtos.forEach((produto) => {
-    const produtoCard = document.createElement("div");
-    produtoCard.classList.add("produtoCard");
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = produto.description;
 
-    produtoCard.innerHTML = `
-        <h3>${produto.title}</h3>
-        <img src="${produto.image}" alt="${produto.title}" class="produtoImagem">
-        <p class="produtoPreco">Custo total: ${produto.price} €</p>
-        <p class="produtoDescricao">${produto.description}</p>
-        <button class="produtoBotao">+ Adicionar ao Cesto</button>
-    `;
-
-    produtosCon.appendChild(produtoCard);
-});
+    article.appendChild(titleElement);
+    article.appendChild(imageElement);
+    article.appendChild(priceElement);
+    article.appendChild(descriptionElement);
+  
+    return article;
+  }
+  
