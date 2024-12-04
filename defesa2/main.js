6e3f03394f0e9a440bbe93998b6968f059af5396
tiago-goncalves-a22201250 <148215009+tiago-goncalves-a22201250@users.noreply.github.com>
2024-12-04 19:27:48 +0000

    1,2,3,4

--- defesa2/main.js
+++ defesa2/main.js
@@ -23,6 +23,8 @@ function exibirProdutos(produtos) {
   let productsContainer = document.getElementById('produtosContainer');
   productsContainer.innerHTML = '';
 
+
+
   produtos.forEach((produto) => {
     const article = criarProduto(produto);
     productsContainer.appendChild(article);
@@ -58,6 +60,36 @@ function criarProduto(produto) {
   article.appendChild(imageElement);
   article.appendChild(priceElement);
   article.appendChild(descriptionElement);
+  article.appendChild(addButton);
+
+  return article;
+}
+
+function criarProdutoSemDescricao(produto) {
+
+  const article = document.createElement('article');
+  article.classList.add('produto');
+
+  const titleElement = document.createElement('h3');
+  titleElement.textContent = produto.title;
+
+  const imageElement = document.createElement('img');
+  imageElement.src = produto.image;
+  imageElement.alt = produto.title;
+
+  const priceElement = document.createElement('p');
+  priceElement.textContent = `Preço: ${produto.price.toFixed(2)} €`;
+  priceElement.classList.add('preco');
+
+  const addButton = document.createElement('button');
+  addButton.textContent = 'Adicionar ao Cesto';
+  addButton.addEventListener('click', function () {
+    adicionarAoCarrinho(produto);
+  });
+
+  article.appendChild(titleElement);
+  article.appendChild(imageElement);
+  article.appendChild(priceElement);
   article.appendChild(addButton);
 
   return article;
@@ -111,21 +143,16 @@ function aplicarFiltros() {
   const categorySelect = document.getElementById('filtro-select');
   const sortSelect = document.getElementById('ordenar-select');
   const searchInput = document.getElementById('search-input');
+  const botaoSelect = document.getElementById('botao-select');
   const botao = document.getElementById('botaoAll');
-    let produtosFiltrados = [...todosOsProdutos];
+
+  let produtosFiltrados = [...todosOsProdutos];
   const categoriaSelecionada = categorySelect.value;
   if (categoriaSelecionada !== 'all') {
     produtosFiltrados = produtosFiltrados.filter(
       (produto) => produto.category === categoriaSelecionada
     );
   }
-
- /* const ordenacaoSelecionada = sortSelect.value;
-  if (ordenacaoSelecionada === 'asc') {
-    produtosFiltrados.sort((a, b) => a.price - b.price);
-  } else if (ordenacaoSelecionada === 'desc') {
-    produtosFiltrados.sort((a, b) => b.price - a.price);
-  }*/
 
   const ordenacaoSelecionada = sortSelect.value;
   if (ordenacaoSelecionada === 'asc') {
@@ -141,6 +168,10 @@ function aplicarFiltros() {
 
   botao.addEventListener('click', function () {
     adicionarAoCarrinhoAll(produtos);
+  });
+
+  botaoSelect.addEventListener('click', function () {
+    criarProdutoSemDescricao(produtos);
   });
 
   exibirProdutos(produtosFiltrados);
